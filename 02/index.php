7a171a25880a2f9c762fc84d2e0140a7f41ff22a
Ecarteo <ecarteocalderon2017@gmail.com>
2026-01-14 15:26:55 -0500

    confirmation pull

--- 02/index.php
+++ 02/index.php
@@ -1,15 +1,36 @@
+<?php
+declare (strict_types = 1);
+
 //1. Set Up & Start 
 
 
 //2. Code Commenting 
 
-<!--  -->
+// 
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
+$firstName = "Matthew";
+$lastName = "Calderon";
+$age = 40;
+$isInstructor = true;
 
+echo "<p> Hello there, my name is " . $firstName . " " . $lastName . "</p";
 
+if ($isInstructor) {
+    echo "<p> I am your teacher <p>";
+}
+else {
+    echo "<p> Opps, teacher didn't show! <p>";
+}
 //4. Loosely Typed Language Demo
+$num1 = 1;
+$num2 = 10;
 
+functions add(int $num1, int $num2) : int {
+    return $num1 + $num2;
+}
+
+echo "<p>" . add($num1, $num2) . "</p>";
 
 //5. Strict Types & Types Hints
 
